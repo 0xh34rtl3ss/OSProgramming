9534fb8c74fb3353f8b748f1009f604a4978c5b3
me-ran <imranmohamad.iium@gmail.com>
2021-06-11 15:51:08 +0800

    update
    1

---
+++ OSProgrammingProject.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include <fstream>
+using namespace std;
+
+
+
+
+int main(){
+
+   // Create a text string, which is used to output the text file
+    string myText;
+
+    // Read from the text file
+    ifstream MyReadFile("inputfile.txt");
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, myText)) {
+    // Output the text from the file
+    cout << myText;
+            }
+
+    // Close the file
+    MyReadFile.close();
+
+
+    
+
+    
+
+    
+    
+
+    return 0;
+}
